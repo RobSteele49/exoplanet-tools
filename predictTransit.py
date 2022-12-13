94a7e5ec8c3eeb04442ef09e7f1c60b72da408af
Rob Steele <robert@steelewoolsoftware.com>
2022-12-13 12:06:30 -0800

    In the middle of debugging using HAT-P-1.

--- predictTransit.py
+++ predictTransit.py
@@ -4,8 +4,8 @@
 # and search for ones that are visible during the night times hours
 # from my location.
 
-# I was to eventually develop a GUI for this program. But for now the
-# funcionality will live in this code.
+# On Dec. 13, 2022 I moved the logic for updating the xml files out of this
+# program and put that logic in the 'updateXmlFiles.py' program.
 
 from astropy import units as u
 
@@ -40,49 +40,6 @@ import time
 
 import xml.etree.ElementTree as ET
 
-# This section of code creates a directory 'xml_files' and then creates
-# softlinks to the xml files. in the open_exoplanet_catalogue in the
-# The goal of this program is to scan all of the trasiting exoplanets
-# and search for ones that are visible during the night times hours
-# from my location.
-
-# I was to eventually develop a GUI for this program. But for now the
-# funcionality will live in this code.
-
-# astropy.time is not working on my Raspberry Pi.
-# This is issue # 3 and more details can be found there.
-
-# This section of code creates a directory 'xml_files' and then creates
-# softlinks to the xml files. in the open_exoplanet_catalogue in the
-# systems and systems_kepler directories. This was done because I could not
-# figure out how to scan though the list of xml files from two directories.
-# The other side benefit was that I could remove xml files that were
-# causing my program to crash.
-
-subprocess.getstatusoutput ('ls')
-subprocess.getstatusoutput ('rm -rf xml_files')
-subprocess.getstatusoutput ('mkdir xml_files')
-
-# The creation of software links was not working for me.
-
-# subprocess.getstatusoutput ('cd xml_files; ln -s ../../OpenExoplanetCatalogue/open_exoplanet_catalogue/systems/* .;cd ..')
-# subprocess.getstatusoutput ('cd xml_files; ln -s ../../OpenExoplanetCatalogue/open_exoplanet_catalogue/systems_kepler/* .;cd ..')
-
-subprocess.getstatusoutput ('cd xml_files; cp ../../OpenExoplanetCatalogue/open_exoplanet_catalogue/systems/*        .;cd ..')
-subprocess.getstatusoutput ('cd xml_files; cp ../../OpenExoplanetCatalogue/open_exoplanet_catalogue/systems_kepler/* .;cd ..')
-
-subprocess.getstatusoutput ('rm xml_files/EPIC?201637175.xml')
-subprocess.getstatusoutput ('rm xml_files/KIC?12557548.xml')
-subprocess.getstatusoutput ('rm xml_files/SDSS?J1110+0116.xml')
-subprocess.getstatusoutput ('rm xml_files/PSO?J318?5-22.xml')
-subprocess.getstatusoutput ('rm xml_files/SIMP0136+0933.xml')
-subprocess.getstatusoutput ('rm xml_files/CFBDSIR2149.xml')
-subprocess.getstatusoutput ('rm xml_files/WISE?0855-0714.xml')
-subprocess.getstatusoutput ('rm xml_files/EPIC?204129699.xml')
-
-# This creates a list of all of the files in systems and systems_kepler.
-# If I can get this working in the 'for file' I won't need the silly
-# softlinks
 
 # As of 2018-08-29 the variable 'fileList' is NOT used in the program.
 
@@ -111,11 +68,17 @@ print ('nowUTC : ', nowUTC, 'UTC')
 startTime = Time(datetime.now(),              scale='utc')
 endTime   = Time(datetime.now()+timedelta(4), scale='utc')
 
+# Overwritting the start and end times with hard wired number is done
+# below. This logic can be commented out when unnecessary.
+
+startTime = Time(datetime(2022,12,14,0,0,0), scale='utc')
+endTime   = Time(datetime(2022,12,23,0,0,0), scale='utc')
+
 print ('startTime: ', startTime)
 print ('endTime  : ', endTime)
 
 observingMorningTime = '04'
-observingEveningTime = '16'
+observingEveningTime = '04' # was 16
 
 print ('Hardwire minMagCutoff, minAltCutoff, and minPlanetStarAreaRatio to 10.0 0.0 0.01')
 
@@ -162,6 +125,13 @@ for file in os.listdir('xml_files'):
             
         for planet in star.findall('.//planet'):
             if planet.findtext ('istransiting') == '1':
+
+# Check for HAT-P-1 as that one is a good one to check the rest of the logic.
+
+                if root.findtext('name') == 'HAT-P-1':
+                    print ('Did find HAT-P-1')
+                    print ('Period       : ', planet.findtext('period'))
+                    print ('Transit Time : ', planet.findtext('transittime'))
 
 # Get the magntiude of the star. Use the visual magnitude if it is available.
 # If not, use the 'B' magnitude and if that isn't available use the
