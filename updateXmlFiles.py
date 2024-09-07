65fab9ceb69e1d0def316e978879f9c3278aad3b
Rob Steele (gmail) <robsteele49@gmail.com>
2024-09-07 13:20:34 -0700

    Updated to run on a windows machine.

--- updateXmlFiles.py
+++ updateXmlFiles.py
@@ -4,27 +4,27 @@
 # 'predictTransit.py' program since there really isn't a need to update
 # the xml files every time I run the predictTransit program.
 
-from astropy import units as u
+#sep from astropy import units as u
 
-import astropy.time
-from astropy.time import Time
-from astropy.time import TimeDelta
+#sep import astropy.time
+#sep from astropy.time import Time
+#sep from astropy.time import TimeDelta
 
-from astropy.coordinates import EarthLocation,SkyCoord
-from astropy.coordinates import AltAz
+#sep from astropy.coordinates import EarthLocation,SkyCoord
+#sep from astropy.coordinates import AltAz
 
-import cmath
+#sep import cmath
 import subprocess
 
-from datetime import timedelta
-from datetime import date
-from datetime import datetime
+#sep from datetime import timedelta
+#sep from datetime import date
+#sep from datetime import datetime
 
 import fnmatch
 
 import os
 
-import time
+#sep import time
 
 import xml.etree.ElementTree as ET
 
@@ -47,24 +47,32 @@ import xml.etree.ElementTree as ET
 # The other side benefit was that I could remove xml files that were
 # causing my program to crash.
 
-subprocess.getstatusoutput ('ls')
-subprocess.getstatusoutput ('rm -rf xml_files')
+subprocess.getstatusoutput ('del xml_files/*')
 subprocess.getstatusoutput ('mkdir xml_files')
 
 # The creation of software links was not working for me.
 
-# subprocess.getstatusoutput ('cd xml_files; ln -s ../../OpenExoplanetCatalogue/open_exoplanet_catalogue/systems/* .;cd ..')
-# subprocess.getstatusoutput ('cd xml_files; ln -s ../../OpenExoplanetCatalogue/open_exoplanet_catalogue/systems_kepler/* .;cd ..')
+subprocess.getstatusoutput ('cd xml_files')
+subprocess.getstatusoutput ('copy ../../OpenExoplanetCatalogue/open_exoplanet_catalogue/systems/*        .')
+subprocess.getstatusoutput ('copy ../../OpenExoplanetCatalogue/open_exoplanet_catalogue/systems_kepler/*y .')
+subprocess.getstatusoutput ('cd ..')
 
-subprocess.getstatusoutput ('cd xml_files; cp ../../OpenExoplanetCatalogue/open_exoplanet_catalogue/systems/*        .;cd ..')
-subprocess.getstatusoutput ('cd xml_files; cp ../../OpenExoplanetCatalogue/open_exoplanet_catalogue/systems_kepler/* .;cd ..')
+subprocess.getstatusoutput ('del "xml_files/CFBDSIR2149.xml"')
+subprocess.getstatusoutput ('del "xml_files/EPIC 201637175.xml"')
+subprocess.getstatusoutput ('del "xml_files/KIC 12557548.xml"')
+subprocess.getstatusoutput ('del "xml_files/SDSS J1110+0116.xml"')
+subprocess.getstatusoutput ('del "xml_files/PSO J318?5-22.xml"')
+subprocess.getstatusoutput ('del "xml_files/SIMP0136+0933.xml"')
+subprocess.getstatusoutput ('del "xml_files/CFBDSIR2149.xml"')
+subprocess.getstatusoutput ('del "xml_files/WISE 0855-0714.xml"')
+subprocess.getstatusoutput ('del "xml_files/EPIC 204129699.xml"')
+subprocess.getstatusoutput ('del "xml_files/KOI-2700.xml"')
+subprocess.getstatusoutput ('del "xml_files/PSO J318.5-22.xml"')
+subprocess.getstatusoutput ('del "xml_files/SDSS J1110+0116.xml"')
+subprocess.getstatusoutput ('del "xml_files/SIMP0136+0933.xml"')
+subprocess.getstatusoutput ('del "xml_files/WISE 0855-0714.xml"')
 
-subprocess.getstatusoutput ('rm xml_files/EPIC?201637175.xml')
-subprocess.getstatusoutput ('rm xml_files/KIC?12557548.xml')
-subprocess.getstatusoutput ('rm xml_files/SDSS?J1110+0116.xml')
-subprocess.getstatusoutput ('rm xml_files/PSO?J318?5-22.xml')
-subprocess.getstatusoutput ('rm xml_files/SIMP0136+0933.xml')
-subprocess.getstatusoutput ('rm xml_files/CFBDSIR2149.xml')
-subprocess.getstatusoutput ('rm xml_files/WISE?0855-0714.xml')
-subprocess.getstatusoutput ('rm xml_files/EPIC?204129699.xml')
+
+
+
 
